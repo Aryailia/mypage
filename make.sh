e2cbe3fa15e111fef7926c4ebf332a13c186d260
Aryailia <Aryailia@users.noreply.github.com>
2021-01-31 04:25:28 +0800

    build-local removes need for ENV shennanigans

--- make.sh
+++ make.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
 
+# We can maybe get rid of this when I implement a basic web server
 wd="$( dirname "${0}"; printf a )"; wd="${wd%?a}"
 wd="$( realpath "${wd}"; printf a )"; wd="${wd%?a}"
 cd "${wd}" || exit 1
-#printf %s\\n "${wd}"
-#DOMAIN="${wd}/website/public" website/make.sh "$@"
-DOMAIN='' "${wd}/website/make.sh" "$@"
+printf %s\\n "Use \`website/make.sh\` for production (i.e. DOMAIN='' make.sh)"
+DOMAIN="${wd}/website/public" website/make.sh "$@"
+
+#DOMAIN='' "${wd}/website/make.sh" "$@"
